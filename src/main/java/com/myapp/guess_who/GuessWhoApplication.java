3727ce47f61bc24170480a8505865adf501d0e38
Dantomaz <daniel.t.mazurczak@gmail.com>
2024-06-23 13:59:32 +0200

    create project

---
+++ src/main/java/com/myapp/guess_who/GuessWhoApplication.java
@@ -0,0 +1,13 @@
+package com.myapp.guess_who;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GuessWhoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GuessWhoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/myapp/guess_who/GuessWhoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.myapp.guess_who;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GuessWhoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
