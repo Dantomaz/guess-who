0f0d9be240186bec1785b4dfa9d81b1db526b7d5
Dantomaz <daniel.t.mazurczak@gmail.com>
2024-11-12 17:12:17 +0100

    remove leftover logs

--- src/main/java/com/myapp/guess_who/session/HttpSessionEventListener.java
+++ src/main/java/com/myapp/guess_who/session/HttpSessionEventListener.java
@@ -5,13 +5,9 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.context.event.EventListener;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.session.Session;
-import org.springframework.session.events.SessionCreatedEvent;
 import org.springframework.session.events.SessionDestroyedEvent;
 import org.springframework.stereotype.Component;
 
-import java.time.Instant;
-import java.time.ZoneId;
-import java.time.temporal.ChronoUnit;
 import java.util.UUID;
 
 @RequiredArgsConstructor
@@ -22,18 +18,7 @@ public class HttpSessionEventListener {
     private final RoomManager roomManager;
 
     @EventListener
-    public void handleSessionCreatedEvent(SessionCreatedEvent event) {
-        System.out.printf("Session %s created%n", event.getSession().getId());
-        System.out.printf("%s - creation time%n%n", event.getSession().getCreationTime().atZone(ZoneId.systemDefault()).toLocalTime());
-    }
-
-    @EventListener
     public void handleSessionDestroyedEvent(SessionDestroyedEvent event) {
-        System.out.printf("Session %s destroyed%n", event.getSession().getId());
-        System.out.printf("%s - creation time%n", event.getSession().getCreationTime().atZone(ZoneId.systemDefault()).toLocalTime());
-        System.out.printf("%s - last accessed time%n", event.getSession().getLastAccessedTime().atZone(ZoneId.systemDefault()).toLocalTime());
-        System.out.printf("%s - current time%n%n", Instant.now().truncatedTo(ChronoUnit.MILLIS).atZone(ZoneId.systemDefault()).toLocalTime());
-
         Session session = event.getSession();
         UUID roomId = session.getAttribute("roomId");
         UUID playerId = session.getAttribute("playerId");
