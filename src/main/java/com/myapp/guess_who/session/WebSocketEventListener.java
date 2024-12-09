a6d1b986f275cb47febd664ae8857b7cce8f7b36
Dantomaz <daniel.t.mazurczak@gmail.com>
2024-12-09 16:04:13 +0100

    react to websocket connection changes

---
+++ src/main/java/com/myapp/guess_who/session/WebSocketEventListener.java
@@ -0,0 +1,65 @@
+package com.myapp.guess_who.session;
+
+import com.myapp.guess_who.room.RoomManager;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.event.EventListener;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
+import org.springframework.messaging.simp.stomp.StompHeaderAccessor;
+import org.springframework.session.Session;
+import org.springframework.stereotype.Component;
+import org.springframework.web.socket.messaging.SessionConnectEvent;
+import org.springframework.web.socket.messaging.SessionDisconnectEvent;
+
+import java.util.Objects;
+import java.util.UUID;
+
+@RequiredArgsConstructor
+@Component
+public class WebSocketEventListener {
+
+    private final RedisSessionService redisSessionService;
+    private final SimpMessagingTemplate messagingTemplate;
+    private final RoomManager roomManager;
+
+    @EventListener
+    public void handleWebSocketConnectEvent(SessionConnectEvent event) {
+        StompHeaderAccessor headerAccessor = StompHeaderAccessor.wrap(event.getMessage());
+        String httpSessionId = Objects.requireNonNull(headerAccessor.getSessionAttributes()).get("SPRING.SESSION.ID").toString();
+        Session httpSession = redisSessionService.getSessionById(httpSessionId);
+
+        if (httpSession == null) {
+            return;
+        }
+
+        UUID roomId = httpSession.getAttribute("roomId");
+        UUID playerId = httpSession.getAttribute("playerId");
+
+        if (roomId == null || playerId == null || !roomManager.roomExists(roomId)) {
+            return;
+        }
+
+        roomManager.changePlayerConnectedStatus(roomId, playerId, true);
+        messagingTemplate.convertAndSend("/topic/room/%s/players".formatted(roomId), roomManager.getRoom(roomId).getPlayers());
+    }
+
+    @EventListener
+    public void handleWebSocketDisconnectEvent(SessionDisconnectEvent event) {
+        StompHeaderAccessor headerAccessor = StompHeaderAccessor.wrap(event.getMessage());
+        String httpSessionId = Objects.requireNonNull(headerAccessor.getSessionAttributes()).get("SPRING.SESSION.ID").toString();
+        Session httpSession = redisSessionService.getSessionById(httpSessionId);
+
+        if (httpSession == null) {
+            return;
+        }
+
+        UUID roomId = httpSession.getAttribute("roomId");
+        UUID playerId = httpSession.getAttribute("playerId");
+
+        if (roomId == null || playerId == null || !roomManager.roomExists(roomId)) {
+            return;
+        }
+
+        roomManager.changePlayerConnectedStatus(roomId, playerId, false);
+        messagingTemplate.convertAndSend("/topic/room/%s/players".formatted(roomId), roomManager.getRoom(roomId).getPlayers());
+    }
+}
